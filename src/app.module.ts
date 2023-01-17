c818a1cc713211153c7797c29472d93db93230bc
Jose <jose.antonio.umeres@gmail.com>
2023-01-16 20:59:11 -0500

    Set up project

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// en prod por lo general no se va querer poner el synchronize en true, porque lo q hace es si hay algun cambio en las entidades automaticamente los sincroniza
+// en prod por lo general se usa migraciones
+// autoLoadEntities es para que cargue automaticamente las entidades que se van definiendo
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
