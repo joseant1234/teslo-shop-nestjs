92a91ab245b0ab09e80d1255f7796805c34d7269
Jose <jose.antonio.umeres@gmail.com>
2023-01-26 21:36:31 -0500

    Added tags prop

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -43,6 +43,12 @@ export class Product {
     @Column('text')
     gender: string;
 
+    @Column('text', {
+        array: true,
+        default: [],
+    })
+    tags: string[];
+
     @BeforeInsert()
     checkSlugInsert() {
         if (!this.slug) {
