34207b0c2dadf676c18eda75031e3e1ed4ac2d6e
Jose <jose.antonio.umeres@gmail.com>
2023-02-11 21:25:36 -0500

    Added guards and decorators to auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, UseGuards, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Headers, SetMetadata, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { IncomingHttpHeaders } from 'http';
 import { AuthService } from './auth.service';
-import { GetUser, RawHeaders } from './decorators';
+import { GetUser, RawHeaders, RoleProtected } from './decorators';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { ValidRoles } from './interfaces';
 
 
 
@@ -41,4 +43,32 @@ export class AuthController {
     }
   }
 
+  // set metadata adiciona informacion extra al metodo, controlador a ejecutar
+  // AuthGuard internamente crea una instancia q la retorna es por eso q no se usa new AuthGuard
+  // no se usa new UserRoleGuard() porque eso haría q cada vez q entre una nueva solicitud crea una nueva instancia
+  @Get('private3')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  testingAnotherPrivateRoute(
+    @GetUser() user: User,
+    @Req() req,
+  ) {
+    return {
+      ok: true,
+      user,
+    }
+  }
+
+  @Get('private5')
+  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  testingAnotherPrivate5Route(
+    @GetUser() user: User,
+    @Req() req,
+  ) {
+    return {
+      ok: true,
+      user,
+    }
+  }
 }
