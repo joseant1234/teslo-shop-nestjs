03e3ff4b79b3bba5bbf6c94c2ce89df836c9dd34
Jose <jose.antonio.umeres@gmail.com>
2023-02-12 21:44:30 -0500

    Update seed module by adding user entity

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
 import { ProductsService } from '../products/products.service';
 import { initialData } from './data/seed-data';
 
@@ -8,20 +11,45 @@ export class SeedService {
 
   constructor(
     private readonly productsService: ProductsService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
   ) {}
 
   async runSeed() {
-    await this.insertNewProducts();
+    await this.deleteTables();
+    const adminUser = await this.insertUsers();
+    await this.insertNewProducts(adminUser);
     return 'SEED EXECUTED';
   }
 
-  private async insertNewProducts() {
+  private async deleteTables() {
+    await this.productsService.deleteAllProducts();
+    const queryBuilder = this.userRepository.createQueryBuilder();
+    await queryBuilder
+      .delete()
+      .where({})
+      .execute()
+  }
+
+  private async insertUsers() {
+    const seedUsers = initialData.users;
+    const users: User[] = [];
+    seedUsers.forEach(user => {
+      users.push(this.userRepository.create(user));
+    });
+    // users no tiene los usuarios con el id, con el save recien se graba en la bd
+
+    const dbUsers = await this.userRepository.save(seedUsers);
+    return dbUsers[0];
+  }
+
+  private async insertNewProducts(user: User) {
     await this.productsService.deleteAllProducts();
     const products = initialData.products;
     const insertPromises = [];
-    // products.forEach(product =>{
-    //   insertPromises.push(this.productsService.create(product));
-    // });
+    products.forEach(product =>{
+      insertPromises.push(this.productsService.create(product, user));
+    });
     await Promise.all(insertPromises);
     return true;
   }
