204161d1a7f39a84a371bd9eb7c1fb4a2c86bf38
Jose <jose.antonio.umeres@gmail.com>
2023-02-17 21:52:16 -0500

    Added events to listen to

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,5 +1,6 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dto/new-message.dto';
 import { MessagesWsService } from './messages-ws.service';
 
 // namespace es como una sala de chat, los clientes se conectan a un namespace
@@ -10,14 +11,32 @@ import { MessagesWsService } from './messages-ws.service';
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
+  // es el socket server y tiene por ejemplo todos los cliente conectado
+  @WebSocketServer() wss: Server;
+
+  // cada vez q un cliente se conecta se ejecuta handleConnection
   handleConnection(client: Socket) {
     // el client.id es único
-    console.log('Client connected: ', client.id)
+    // console.log('Client connected: ', client.id)
+    this.messagesWsService.registerClient(client);
+    // el client.emit solo emite un mensaje al cliente q se conectó
+    // wss.emit emite a todos los clientes conectados
+    // emit('nombre del evento', envia cualquier valor q el cliente requiera)
+    this.wss.emit('client-updated', this.messagesWsService.getConnectedClients());
   }
 
   handleDisconnect(client: Socket) {
-    console.log('Client desconnected: ', client.id)
+    // console.log('Client desconnected: ', client.id)
+    this.messagesWsService.removeClient(client.id);
+    this.wss.emit('client-updated', this.messagesWsService.getConnectedClients());
   }
 
+  // message-from-client nombre del evento q se está escuchando
+  // client es el q socket q está emitiendo el evento
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
+    console.log(client.id)
+    console.log(payload)
+  }
 
 }
