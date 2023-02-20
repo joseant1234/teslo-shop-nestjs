a8af2139ae33f5c6a7631e95a33e39dc50b8786f
Jose <jose.antonio.umeres@gmail.com>
2023-02-19 21:12:54 -0500

    Updated socket

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,5 +1,7 @@
+import { JwtService } from '@nestjs/jwt';
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { JwtPayload } from 'src/auth/interfaces';
 import { NewMessageDto } from './dto/new-message.dto';
 import { MessagesWsService } from './messages-ws.service';
 
@@ -9,20 +11,38 @@ import { MessagesWsService } from './messages-ws.service';
 // cada cliente tiene un identificador único y si tiene especificado un namespace se conectan a dos namespace al root y al q está especificado en el namespace
 @WebSocketGateway({ cors: true, namespace: '' })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   // es el socket server y tiene por ejemplo todos los cliente conectado
   @WebSocketServer() wss: Server;
 
   // cada vez q un cliente se conecta se ejecuta handleConnection
-  handleConnection(client: Socket) {
+  async handleConnection(client: Socket) {
     // el client.id es único
     // console.log('Client connected: ', client.id)
-    this.messagesWsService.registerClient(client);
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      // tambien se puede manejar los errores con los exeception de ws con nest
+      client.disconnect();
+      return;
+    }
+
     // el client.emit solo emite un mensaje al cliente q se conectó
     // wss.emit emite a todos los clientes conectados
     // emit('nombre del evento', envia cualquier valor q el cliente requiera)
     this.wss.emit('client-updated', this.messagesWsService.getConnectedClients());
+
+    // cuando se conecta tambien lo hace al canal de ventas
+    // client.join('ventas')
+    // emite a todos los usuarios en el canal de ventas
+    // this.wss.to('ventas').emit('')
   }
 
   handleDisconnect(client: Socket) {
@@ -35,8 +55,24 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   // client es el q socket q está emitiendo el evento
   @SubscribeMessage('message-from-client')
   onMessageFromClient(client: Socket, payload: NewMessageDto) {
-    console.log(client.id)
-    console.log(payload)
+    // con client.emit se emite al cliente q envió el mensaje
+    // client.emit('message-from-server', {
+    //   fullName: 'Full name',
+    //   message: payload.message || 'no-message'
+    // })
+
+    // emitir a todos menos al cliente q envió el mensaje
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'Full name',
+    //   message: payload.message || 'no-message'
+    // })
+
+    // emitie a todos incluyendo el cliente q envió el mensaje
+    this.wss.emit('message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload.message || 'no-message'
+    })
+
   }
 
 }
